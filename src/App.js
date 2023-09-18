8f7b30e8892323660df4dee5e4c906990bab6c5b
Per Ola Stålberg <olastaalberg@gmail.com>
2023-09-18 13:55:47 +0200

    adding button to get token in clipboard

--- src/App.js
+++ src/App.js
@@ -11,9 +11,72 @@ import { useAuth } from "react-oidc-context"
 const App = () => {
    const [username, setUsername] = useState('');
    const auth = useAuth();
+   const [awaitTokenLoader, setAwaitTokenLoader] = useState(false); 
 
    const idPortenButton = auth.isAuthenticated ? <Button onClick={auth.signoutRedirect }>Logg ut av ID-porten</Button> : <Button onClick={auth.signinRedirect}>ID-porten logg inn</Button>;
+   const getTokenButton = auth.isAuthenticated ? awaitTokenLoader ?  <div class="loader"></div> : <Button onClick={handleTokenClipboardAlert}>Kopier Altinntoken</Button> : <></>;
 
+   async function handleTokenClipboardAlert(){
+      setAwaitTokenLoader(true);
+      var token = await ExchangeAltinnToken();
+      if (token != null){
+         navigator.clipboard.writeText(token);
+         alert("Altinntoken er kopiert til din utklippstavle!")
+      }else{
+         alert("Noe gikk galt ved utveksling av Altinntoken.")
+      }
+      setAwaitTokenLoader(false);
+   }
+
+   async function ExchangeAltinnToken() {
+
+      const idPortenTokenBearer = auth.user?.access_token;
+      const bearerPayload = {
+         "idPortenToken": idPortenTokenBearer
+      };
+      let altinnToken = null;
+      try {
+         altinnToken = await AltinnExchangeCall(bearerPayload);
+
+         if (altinnToken == null) {
+            try {
+               const newBearer = {
+                  "idPortenToken": auth.user?.access_token
+               };
+               altinnToken = await AltinnExchangeCall(newBearer);
+            } catch (exception) {
+               throw exception;
+            }
+         } else {
+            return altinnToken;
+         }
+      } catch {
+         return null;
+      }
+   }
+
+   async function AltinnExchangeCall(bearerPayload) {
+      try {
+         const response = await fetch("https://dibk.apps.tt02.altinn.no/dibk/varselplanoppstart/exchangealtinntoken", {
+            method: "post",
+            body: JSON.stringify(bearerPayload),
+            headers: {
+               'Content-Type': 'application/json',
+            }
+         });
+         let altinnToken = null;
+
+         if (response.status === 200 || response.status === 201) {
+            await response.text().then(function (data) {
+               altinnToken = data;
+            });
+         }
+         return altinnToken;
+      } catch (e) {
+         return null;
+      }
+   }
+  
    return (
       <Provider store={store}>
          <div className="App">
@@ -44,8 +107,10 @@ const App = () => {
                   <div className="col-1">
                      {idPortenButton}
                   </div>
+                  <div className="col-1">
+                     {getTokenButton}
+                  </div>
                </div>
-
 
                <div className="app-container">
                   <Tabs defaultActiveKey="varsel-om-planoppstart" id="tabs" transition={false}>
